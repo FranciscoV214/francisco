a013238f49cf43c2cf7421612d72aacc7b5c8704
FranciscoV214 <axel.francisco.villarreal@gmail.com>
2025-10-29 14:27:03 -0300

    Borré los archivos pq fueron para boludear...

---
+++ Práctica C++/gestion_archivos_practica1.cpp
@@ -0,0 +1,10 @@
+#include <fstream>  // Para ofstream
+#include <iostream> // Para cout
+using namespace std;
+
+int main ()
+{
+
+ifstream archivo1("documento1.txt");
+
+}
--- Práctica Python/hello_world.py
+++
@@ -1,3 +0,0 @@
-nombre = input ("¿Cual es tu nombre? \n")
-
-print ("Parece que tu nombre es: ", nombre)
