b6633f0c06115ee5de471e8cf252dd32da5b32af
FranciscoV214 <axel.francisco.villarreal@gmail.com>
2025-10-30 11:32:55 -0300

    Implementación de primeras funciones

---
+++ Práctica Python/ejercicio_funciones_parte2.py
@@ -0,0 +1,14 @@
+def suma_implementacion1(a,b):
+    num1=a
+    num2=b
+    return(num1+num2)
+#Una función se puede implementar devolviendo un valor con return()
+
+def suma_implementacion2(a,b):
+    print(a+b)
+#Otra forma de implementar lo anterior es simplemente imprimir el resultado en consola sin devolver nada
+
+sumanumeros1=suma_implementacion1(5,7)
+print(sumanumeros1)
+
+suma_implementacion2(5,7)
