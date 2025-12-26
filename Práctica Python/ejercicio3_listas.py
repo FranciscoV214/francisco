ab7f7f6c517f4efbcc22091fd6c8ea84434d4dcc
FranciscoV214 <axel.francisco.villarreal@gmail.com>
2025-12-26 12:14:10 -0300

    Ni idea de que hice acá jajaja

---
+++ Práctica Python/ejercicio3_listas.py
@@ -0,0 +1,3 @@
+miLista=["Francisco", 2, 25.3, True, "Curso - Píldoras Informáticas"]
+miLista.extend(["nuevo_elemento1","nuevo_elemento2","nuevo_elemento3"])
+print(miLista[:])
---
+++ Práctica Python/ejercicio4_listas.py
@@ -0,0 +1,2 @@
+miLista=["Francisco", 2, 25.3, True, "Curso - Píldoras Informáticas"]
+print(miLista.index(25.3))
